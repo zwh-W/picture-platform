dd90a8ab90dbf9ef4dc94962a24bbfee89708c2f
张蘶豪 <1840816813@qq.com>
2025-07-13 10:21:00 +0800

    feat: 集成WebSocket实时通信
    - 配置WebSocket支持
    - 创建WebSocket配置类
    - 实现握手拦截器
    - 添加WebSocket消息模型
    - 支持多端实时通信
    - 实现图片协同编辑功能
    - 集成Disruptor高性能队列

---
+++ src/main/java/com/zwh/manager/websocket/disruptor/PictureEditEvent.java
@@ -0,0 +1,34 @@
+package com.zwh.manager.websocket.disruptor;
+
+import com.zwh.manager.websocket.model.PictureEditRequestMessage;
+import com.zwh.model.entity.User;
+import lombok.Data;
+import org.springframework.web.socket.WebSocketSession;
+
+/**
+ * 图片编辑事件
+ */
+@Data
+public class PictureEditEvent {
+
+    /**
+     * 消息
+     */
+    private PictureEditRequestMessage pictureEditRequestMessage;
+
+    /**
+     * 当前用户的 session
+     */
+    private WebSocketSession session;
+
+    /**
+     * 当前用户
+     */
+    private User user;
+
+    /**
+     * 图片 id
+     */
+    private Long pictureId;
+
+}
